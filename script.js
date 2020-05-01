f421f38329c77fbdb72b2ea46b2fc6b2bcf0e039
Joey Reyes <joeyr1984@gmail.com>
2020-05-01 10:13:33 -0700

    colors past , presnt , and future is now working

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-var timeSlots = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "8pm", "9pm"];
+//var timeSlots = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "8pm", "9pm"];
+var timeSlots = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21];
 
-$(document).ready(function(){
+$(document).ready(function () {
     $("#currentDay").html(moment().format("dddd, MMMM Do"));
-    var currentTime = moment().format("ha");
+    //var currentTime = moment().format("ha");
+    var currentTime = moment();
     timeSlots.forEach(hour => {
-       var hourObj = moment(moment().format("L") + " " + hour);
-        if (currentTime == hour) {
-            $("#new-task-" + hour).addClass("present");
+        var hourObj = moment().set('hour', hour);
+        var hourFormat = moment(hourObj).format("ha");
+        if (moment(hourObj).isBefore(currentTime, "hour")) {
+            $("#new-task-" + hourFormat).addClass("past");
             console.log(hourObj);
-        //} else if (moment(hour).isBefore(currentTime)){
+
+            //} else if (moment(hour).isBefore(currentTime)){
             //$("#new-task-" + hour).addClass("past");
+        } else if (moment(hourObj).isSame(currentTime, "hour")) {
+            $("#new-task-" + hourFormat).addClass("present");
+        } else if (moment(hourObj).isAfter(currentTime, "hour")) {
+            $("#new-task-" + hourFormat).addClass("future");
         }
+        
     });
 });
 //var pastTime
